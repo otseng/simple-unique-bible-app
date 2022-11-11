bc1a5006a896d350bd76c662272dbc278fb91628
Oliver Tseng <otseng@yahoo.com>
2022-11-10 22:36:50 -0500

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Simple Unique Bible Viewer'
 // export const API_SERVER = 'http://localhost:8080'
-export const API_SERVER = 'http://216.117.154.108:7777'
+export const API_SERVER = 'https://p4um.com/api'
