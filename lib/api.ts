bde94e6ed2dd1ab986674db418f8a4c88762e6e8
Oliver Tseng <otseng@yahoo.com>
2022-11-11 07:25:54 -0500

    Add basic auth

--- lib/api.ts
+++ lib/api.ts
@@ -2,9 +2,15 @@ import axios from 'axios'
 import useSWR from 'swr'
 import { API_SERVER } from './constants';
 
+const auth = {
+  username: 'ubaclient',
+  password: 'uniquebibleapp'
+}
+
 export function getBibles() {
+  
   const address = API_SERVER + '/bible';
-  const fetcher = async (url) => await axios.get(url).then((res) => res.data.data);
+  const fetcher = async (url) => await axios.get(url, {auth}).then((res) => res.data.data);
   const { data, error } = useSWR(address, fetcher);
 
   return {
@@ -16,7 +22,7 @@ export function getBibles() {
 
 export function getBibleBooks() {
   const address = API_SERVER + '/data/bible/abbreviations?lang=eng';
-  const fetcher = async (url) => await axios.get(url).then((res) => res.data.data);
+  const fetcher = async (url) => await axios.get(url, {auth}).then((res) => res.data.data);
   const { data, error } = useSWR(address, fetcher);
 
   return {
@@ -28,7 +34,7 @@ export function getBibleBooks() {
 
 export function getBibleChapter(text, bookNumber, chapter) {
   const address = API_SERVER + `/bible/${text}/${bookNumber}/${chapter}`;
-  const fetcher = async (url) => await axios.get(url).then((res) => res.data.data);
+  const fetcher = async (url) => await axios.get(url, {auth}).then((res) => res.data.data);
   const { data, error } = useSWR(address, fetcher);
 
   return {
