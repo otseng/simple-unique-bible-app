a082aee16ea02051a3aa3cafafc3a28dc88f833f
Oliver Tseng <otseng@yahoo.com>
2022-11-18 09:21:01 -0500

    Add version

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,17 @@
+import React from 'react';
 import Container from './container'
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 const Footer = () => {
-
-  const codeRepoPath = 'https://github.com/otseng/simple-unique-bible-viewer'
 
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            &nbsp;
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://github.com/eliranwong/UniqueBible" target="new"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Unique Bible App
-            </a>
-            <a
-              href={`${codeRepoPath}`} target="new"
-              className="mx-3 font-bold hover:underline"
-            >
-              GitHub code repo
-            </a>
-          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center pt-5 pb-5 text-sm font-bold text-slate-400">
+            Ver {publicRuntimeConfig?.version} &nbsp;&bull;&nbsp;
+            <a href="https://github.com/eliranwong/UniqueBible" target="new"> Unique Bible App </a>
         </div>
       </Container>
     </footer>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,4 @@
 import isDev from "./util"
 
 export const APP_NAME = 'Simple Unique Bible Viewer'
-export const API_SERVER = isDev() ? 'http://localhost:8080' : 'https://uniquebibleapp.com/api'
-
+export const API_SERVER = isDev() ? 'http://localhost:8080' : 'https://simple.uniquebibleapp.com/api'
