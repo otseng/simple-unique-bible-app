904d0787411bb613d338b696daa78c2d5bcaa5ae
Oliver Tseng <otseng@yahoo.com>
2022-11-17 23:19:15 -0500

    Add about

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,13 +3,8 @@ import Link from 'next/link'
 
 const Intro = () => (
   <section className="flex-row flex items-center justify-between mt-10 mb-10">
-    <div className="text-2xl md:text-3xl font-bold text-blue-400 hover:text-blue-600">
-      <Link href="/">
-        Simple Unique Bible Viewer
-      </Link>
-    </div>
-    {/* <div className="justify-self-end object-top">Menu</div> */}
-    <Popover className="relative">
+
+<Popover className="relative">
       <Popover.Button className="dropdown-toggle inline-block px-4 py-2.5 bg-indigo-300 hover:bg-indigo-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg active:text-white transition duration-150 ease-in-out flex items-center whitespace-nowrap"
       >
         <div className="space-y-1 rounded">
@@ -18,16 +13,37 @@ const Intro = () => (
           <span className="block w-4 h-1 bg-gray-600"></span>
         </div>
       </Popover.Button>
+      <Transition
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
       <Popover.Panel className="absolute z-10">
         <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5
-        min-w-100">
+        w-200">
           <div className="relative grid gap-1 bg-indigo-300 p-3 rounded">
+            {(`${window.location.pathname}` != '/') &&
             <Popover.Button className="hover:bg-indigo-400">
               <Link href="/">Home</Link>
             </Popover.Button>
+            }
+            <Popover.Button className="hover:bg-indigo-400">
+              <Link href="/about">About</Link>
+            </Popover.Button>
           </div></div>
       </Popover.Panel>
+      </Transition>
     </Popover>
+
+    <div className="text-2xl md:text-3xl font-bold text-blue-400 hover:text-blue-600">
+      <Link href="/">
+        Simple Unique Bible Viewer
+      </Link>
+    </div>
+
   </section>
 )
 
---
+++ pages/about.tsx
@@ -0,0 +1,63 @@
+import Container from '../components/container'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import Head from 'next/head'
+import Link from 'next/link';
+import { getBibles, getBooks, getCommentaries } from '../lib/api'
+import Spinner from 'react-bootstrap/Spinner';
+import { APP_NAME } from '../lib/constants';
+import { clickableButton, homeDisclosure, nonclickableButton } from '../lib/styles';
+import { scrollToTop } from '../lib/util';
+import { useEffect, useState } from 'react';
+import { Disclosure } from '@headlessui/react';
+
+export default function Index() {
+
+  const [showScrollToTopButton, setShowScrollToTopButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowScrollToTopButton(true);
+      } else {
+        setShowScrollToTopButton(false);
+      }
+    });
+  }, []);
+
+return (
+    <>
+      <Layout>
+        <Head>
+          <title>{APP_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+
+          <div className="text-xl"><button className={`${nonclickableButton}`}>About</button></div>
+
+          <div className="m-10">
+            
+            <p>Simple web viewer for <a target="new" href="https://github.com/eliranwong/UniqueBible">UniqueBibleApp</a> written in React/Next.</p>
+
+            <p>&nbsp;</p>
+
+            <p className="text-lg font-bold">More UBA viewers:</p>
+            <ul>
+              <li><a target="new" href="https://transliteralbible.com/index.html?cmd=.bible">TransliteralBible.com</a></li>
+              <li><a target="new" href="https://bible.gospelchurch.uk/index.html?cmd=John%203:16">GospelChurch.uk</a></li>
+            </ul>
+            
+          </div>
+
+          {showScrollToTopButton && (
+            <button onClick={scrollToTop} className="back-to-top">
+              &#8679;
+            </button>
+          )}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
