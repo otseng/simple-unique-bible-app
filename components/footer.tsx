a401e9ee6a70570da77bb70d039fe2b9803e614a
Oliver Tseng <otseng@yahoo.com>
2022-11-18 09:38:54 -0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-          <div className="flex flex-col lg:flex-row justify-center items-center pt-5 pb-5 text-sm font-bold text-slate-400">
-            Version: {version} &nbsp;&bull;&nbsp;
+          <div className="flex justify-center items-center pt-5 pb-5 text-sm font-bold text-slate-400">
+            Version: {version} &bull;&nbsp;
             <a href="https://github.com/eliranwong/UniqueBible" target="new">Unique Bible App</a>
         </div>
       </Container>
