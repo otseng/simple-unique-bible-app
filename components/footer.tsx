ce67be5fac5343b1ead619b6c20ab7fc830efbce
Oliver Tseng <otseng@yahoo.com>
2022-11-18 22:36:25 -0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,8 +13,8 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
           <div className="flex justify-center items-center pt-5 pb-5 text-sm font-bold text-slate-400">
-            {isDev() && <span>(Dev Mode)</span>}
-            Version: {version} &bull;&nbsp;
+            {isDev() && <span>(Dev Mode)&nbsp;</span>}
+            <a href="https://github.com/otseng/simple-unique-bible-viewer/blob/main/CHANGELOG.md" target="new">Version: {version}</a>&nbsp;&bull;&nbsp;
             <a href="https://github.com/eliranwong/UniqueBible" target="new">Unique Bible App</a>
         </div>
       </Container>
