21f2dae0d655d4efa07408ab7f46d674d9f72124
Oliver Tseng <otseng@yahoo.com>
2022-12-09 20:25:43 -0500

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ import { isDev } from "./util"
 
 export const APP_NAME = 'Simple Unique Bible Viewer'
 export const DOMAIN = 'https://simple.uniquebibleapp.com'
-export const API_SERVER = isDev() ? 'http://localhost:8080' : DOMAIN + '/api'
-// export const API_SERVER = DOMAIN + '/api'
+// export const API_SERVER = isDev() ? 'http://localhost:8080' : (DOMAIN + '/api')
+export const API_SERVER = DOMAIN + '/api'
