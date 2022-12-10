8e40e4da2617e14c59e63baf866e8d531c7a82eb
Oliver Tseng <otseng@yahoo.com>
2022-12-09 20:43:57 -0500

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -175,12 +175,13 @@ export function getCompareVerses(book, chapter, verse) {
       address +=
         '&text=ESV&text=NASB&text=NIV&text=AMP&text=TLB&text=TPT&text=MSG'
     }
-    console.log("compare 1: " + address)
+    console.log("compare")
     address +=
       '&text=ERV&text=ISV&text=ULT&text=UST&text=2001' +
       '&text=BBE&text=EasyEnglish&text=NHEB&text=PESH' +
       '&text=ASV&text=LEB' +
       '&text=YLT&text=Darby&txt=KJV1611&text=Geneva&text=Wesley&text=Bishops&text=Wycliffe'
+    console.log("compare")
     address +=
       '&text=TRLIT&text=TRLITx' +
       '&text=KJVx&text=HKJVx' +
@@ -188,6 +189,7 @@ export function getCompareVerses(book, chapter, verse) {
     if (isPowerMode()) {
       address += '&text=NASBx'
     }
+    console.log("compare: " + address)
     address +=
       '&text=OHGB' +
       '&text=Tanakhxx&text=MOB' +
