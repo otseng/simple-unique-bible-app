a53ec7adea49ba88c29a3a53cf69203186efc963
Oliver Tseng <otseng@yahoo.com>
2023-05-29 10:04:38 -0400

    Fix header

--- components/intro.tsx
+++ components/intro.tsx
@@ -24,7 +24,7 @@ const Intro = (props) => {
   menuItems.push([lang.About, '/about'])
   
   return (
-  <section className="flex-row flex items-center justify-between mt-10 mb-10">
+  <section className="flex-row flex items-center justify-between mt-10 mb-10 sticky top-0 z-50 bg-blue-300 rounded">
 
     <Popover className="relative">
       <Popover.Button className="dropdown-toggle inline-block px-4 py-2.5 bg-blue-300 hover:bg-blue-400 -5text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg active:text-white transition duration-150 ease-in-out flex items-center whitespace-nowrap"
@@ -76,7 +76,7 @@ const Intro = (props) => {
       </Transition>
     </Popover>
 
-    <div className="text-2xl md:text-3xl font-bold text-blue-400 hover:text-blue-600">
+    <div className="text-2xl md:text-3xl font-bold text-blue-400 hover:text-blue-600 mr-10">
       <Link href="/">
         Simple Unique Bible Viewer
       </Link>
