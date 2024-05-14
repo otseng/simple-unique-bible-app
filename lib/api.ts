8549fa9dd9d9b292ac424456517d02ef8b8b8615
Oliver Tseng <otseng@yahoo.com>
2024-05-14 06:39:47 -0400

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -190,28 +190,17 @@ export function getCompareVerses(book, chapter, verse) {
     address += '&text=KJV&text=WEB&text=NET'
     if (powerMode) {
       address +=
-        '&text=ESV&text=NASB&text=NIV&text=AMP&text=TLB&text=TPT&text=MSG'
-      console.log(address)
+        '&text=ESV&text=NASB&text=NIV&text=NLT&text=NRSV&text=AMP&text=TLB&text=TPT&text=MSG'
     }
     address = address +
       '&text=ERV&text=ISV&text=ULT&text=UST&text=2001' +
       '&text=BBE&text=EasyEnglish&text=NHEB&text=PESH' +
       '&text=ASV&text=LEB&text=DRB' +
       '&text=YLT&text=Darby&text=KJV1611&text=Geneva&text=Bishops&text=Coverdale&text=Tyndale&text=Wycliffe'
-    console.log(address)
     address = address +
       '&text=TRLIT&text=TRLITx' +
       '&text=KJVx'
-      // '&text=ASVx&text=LEBx&text=WEBx&text=NETx'
-    // if (powerMode) {
-    //   address += '&text=NASBx'
-    // }
-    // address = address +
-    //   '&text=OHGB' +
-    //   '&text=Tanakhxx&text=MOB' +
-    //   '&text=Greek%2b&text=TRx' +
-    //   '&text=CUV&text=Pinyin'
-    // console.log(address)
+    console.log(address)
   } else if (getLang().startsWith("zh")) {
     address +=
       '&text=CUV&text=CUVs&text=Pinyin' +
