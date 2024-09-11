140e71724323a2cb40a2c1211dba34d9697982a1
Oliver Tseng <otseng@yahoo.com>
2024-09-11 18:57:35 -0400

    Change viewer to app

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 import { isDev } from "./util"
 
-export const APP_NAME = 'Simple Unique Bible Viewer'
+export const APP_NAME = 'Simple Unique Bible App'
 export const DOMAIN = 'https://simple.uniquebibleapp.com'
 export const API_SERVER = isDev() ? 'http://localhost:8080' : (DOMAIN + '/api')
 // export const API_SERVER = DOMAIN + '/api'
