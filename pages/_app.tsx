49c25f0b4bccd433d86ed98972edf5dea37f2bf7
Oliver Tseng <otseng@yahoo.com>
2024-11-25 06:43:22 -0500

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import { AppProps } from 'next/app'
 import { LangProvider } from '../lang/langContext'
 import { ThemeProvider } from '../theme/themeContext'
 import '../styles/index.css'
+import ReactGA from "react-ga4";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-  return (
-    <ThemeProvider>
-      <LangProvider>
-        <Component {...pageProps} />
-      </LangProvider>
-    </ThemeProvider>
+    ReactGA.initialize("G-GZTFH5YKRF");
 
-  )
+    return (
+        <ThemeProvider>
+        <LangProvider>
+            <Component {...pageProps} />
+        </LangProvider>
+        </ThemeProvider>
+
+    )
 }
